d1330493fa7b42e18208d305aa8755c8d5070c62
nice-switch <kevin@senpai.zone>
2023-12-19 16:20:27 -0500

    Documenting as I write, double checking how I'm structuring. I like the enum stuff.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
 import odv, sys
 
+# Checks for available args and grabs the first one if available.
+# NOTE sys.argv[0] is the file arg.
+target_service: str | None = len(sys.argv) >= 1 and sys.argv[1] or None
+
+# Validity check for target_service, this should always be something!
+if (target_service is None):
+    # TODO add available args list with error message.
+    raise Exception("No target service to initialize...")
+
+# Just some post processing on the string.
+target_service = target_service.lower()
+
+# Checks if more args are available after target_service then creates a list with the extra args or a empty list when none are available.
+cli_args: list[str] = len(sys.argv) > 2 and sys.argv[3::] or []
+
+
 # TODO Make a TOML/JSON file for all configuration.
 
-service: str = sys.argv[1]
-args: list[str] = sys.argv[2::]
 
-# I'm being too extra and shotgunning things.
-# I'm going to refine the CLI & Enums.
-
-
+match target_service:
+    case odv.enum.ServiceType.DATABASE:
+        print("development development!")
+    
+    
---
+++ src/odv/enum.py
@@ -0,0 +1,13 @@
+from enum import StrEnum
+
+class ServiceType(StrEnum):
+    DATABASE = "database"
+    CLIENT = "client"
+    GUARD = "guard"
+    WEB = "web"
+
+class DatabaseType(StrEnum):
+    DEVELOPMENT = "development"
+    PRODUCTION = "production"
+
+
