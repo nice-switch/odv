9100114ecef3c6863962bfdf52abd4f8e9621882
nice-switch <kevin@senpai.zone>
2023-12-20 06:05:04 -0500

    Commenting documentation and such.

--- src/odv/enum.py
+++ src/odv/enum.py
@@ -2,22 +2,13 @@ import socket
 
 from enum import StrEnum, IntEnum
 
-
 class DatabaseType(StrEnum):
-    """_summary_
-
-    Args:
-        StrEnum (_type_): _description_
-    """
+    """Types of databases that are available to use."""
     PRODUCTION = "production"
     DEVELOPMENT = "development"
 
 class ServiceType(StrEnum):
-    """_summary_
-
-    Args:
-        StrEnum (_type_): _description_
-    """
+    """Types of services that are available to use."""
     DATABASE = "database"
     CLIENT = "client"
     GUARD = "guard"
@@ -25,20 +16,12 @@ class ServiceType(StrEnum):
 
 # TODO add config loader for DatabaseHost & DatabasePort    
 class DatabaseHost(StrEnum):
-    """_summary_
-
-    Args:
-        StrEnum (_type_): _description_
-    """
+    """Available IPs to host the API."""
     MACHINE = "127.0.0.1"
     LOCALHOST = socket.gethostbyname(socket.gethostname())
 
 
 class DatabasePort(IntEnum):
-    """_summary_
-
-    Args:
-        IntEnum (_type_): _description_
-    """
+    """Available ports for the API."""
     PRODUCTION = 6000
     DEVELOPMENT = 6500
