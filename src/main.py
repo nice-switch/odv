f9c89ab5ee2d2df9c54e633771108a80d418deba
nice-switch <kevin@senpai.zone>
2023-12-23 14:31:02 -0500

    Unexpected stuff....

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@
 # NOW HEAR ME OUT!
 
 """
-    I started working on the database API and I got hit with a roadblock with how I"m going to deal with authorization.
-    I need to setup authorization first, then i can work on the API.
-    Sorry gang.
+    OKAY SO...
+    remember how i said i was going to do things?...
 """
